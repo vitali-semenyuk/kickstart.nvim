6587875b3a8f17ca9b6ad48746784eb9fcf936f2
Vitali Semenyuk <vitali.semenyuk@gmail.com>
2026-04-08 15:03:14 +0200

    Add ruby and ts lsp

--- init.lua
+++ init.lua
@@ -617,7 +617,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        ruby_lsp = {},
 
         stylua = {}, -- Used to format Lua code
 
